f5e15f05d792ae6fc7d85aa2b28323102c3d0ba8
ROOTA5351 <roota5351@bose.hpc.uwec.edu>
2024-12-09 15:02:28 -0600

    semsim training prep

---
+++ batch_scripts/train_semsim.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+# ---- SLURM SETTINGS ---- #
+
+# -- Job Specific -- #
+
+#SBATCH --job-name="Train SemSim Model"     # What is your job called?
+
+#SBATCH --output=output/train_semsim.txt     # Output file - Use %j to inject job id, like output-%j.txt
+
+#SBATCH --error=error/train_semsim.txt       # Error file - Use %j to inject job id, like error-%j.txt
+
+
+
+#SBATCH --partition=GPU        # Which group of nodes do you want to use? Use "GPU" for graphics card support
+
+#SBATCH --time=7-00:00:00       # What is the max time you expect the job to finish by? DD-HH:MM:SS
+
+
+# -- Resource Requirements -- #
+
+
+#SBATCH --mem=64G                # How much memory do you need?
+
+#SBATCH --ntasks-per-node=16     # How many CPU cores do you want to use per node (max 64)?
+
+#SBATCH --nodes=1               # How many nodes do you need to use at once?
+
+#SBATCH --gpus=2               # How many GPUs do you need (max 3)? Remove first "#" to enable
+
+# -- Email Support -- #
+
+
+
+#SBATCH --mail-type=END                # What notifications should be emailed about? (Options: NONE, ALL, BEGIN, END, FAIL, QUEUE)
+
+
+# ---- YOUR SCRIPT ---- #
+
+python3.9 ./py_code/train_semsim.py
